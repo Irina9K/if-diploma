b20fddc1bf57ae158a4c347b1e0bcaa8f7f12f62
Irina9K <irina121291992@gmail.com>
2022-10-20 23:25:40 +0300

    added header with style

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -4,26 +4,48 @@ import './header.css';
 import '../../assets/css/style.css';
 
 const Header = () => (
-  <div className="header_container container">
+  <header>
+    <div className="header_container container">
+      <div className="wrapper_logo_search">
+        <div className="header_logo">
+          <svg className="logo_icon">
+            <use xlinkHref={`${IconsSVG}#logo_fox_library`} />
+          </svg>
+        </div>
 
-    <div className="header__logo">
-      <svg className="logo_icon">
-        <use xlinkHref={`${IconsSVG}#logo_fox_library`} />
-      </svg>
+        <input
+          className="input_search"
+          type="search"
+          name="search"
+          placeholder="Search by author, title, name"
+        ></input>
+      </div>
+
+      <nav className="nav">
+        <ul className="nav_list">
+          <div className="nav_logout">
+            <li className="nav_item">Log In</li>
+            <li className="nav_item">Sign up</li>
+          </div>
+
+          <div className="nav_login">
+            <li className="nav_item">All books</li>
+            <li className="nav_item">Your orders</li>
+            <li>
+              <svg className="rectangle_user_icon">
+                <use xlinkHref={`${IconsSVG}#rectangle_user`} />
+              </svg>
+            </li>
+            <li>
+              <svg className="arrow_icon">
+                <use xlinkHref={`${IconsSVG}#arrow`} />
+              </svg>
+            </li>
+          </div>
+        </ul>
+      </nav>
     </div>
-
-    <input
-      className="input_search"
-      type="search"
-      name="search"
-      placeholder="Search by author, title, name"
-    ></input>
-
-    <div className="name_log">
-      <p>Log In</p>
-      <p>Log Out</p>
-    </div>
-  </div>
+  </header>
 );
 
 export default Header;
